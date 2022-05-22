dd60a61ac3eb04e9247d0ee47d6221e1589b0f2a
Biolexey <Biolexey@gmail.com>
2022-05-22 15:47:46 +0900

    add parameter config

--- interactiveBezier.py
+++ interactiveBezier.py
@@ -8,6 +8,12 @@ order = int(input("Input the Bezier Curve's order -> "))#次元数の入力
 NP = order+1                             # n次ベジエ曲線の制御点数
 mode = int(input("Type of calculating(Recursive=1,Iterative=2,Bernstein=3) -> "))
                                          # 計算モード選択
+ab = list(map(float, input("Put the parameter t's range(0->1) -> ").split()))
+                                         # パラメータtの始点と終点
+if len(ab) < 2:
+  a, b = 0, 1                            # パラメータ入力がない場合は0,1で初期化
+else:
+  a, b = ab[0], ab[1]
 
 def norm(v):                             # ベクトルのノルム計算
   '''
@@ -26,7 +32,7 @@ def pressed1(event):                     # Button1 pressed コールバック関
     canvas.drawMarker(newpnt)            # 新しい制御点の描画
     if len(points) == NP:                # 制御点数が n+1個に到達
       canvas.clear()                     # canvasのクリア
-      BezierCurve(canvas, points, mode).drawCurve() # n次ベジエ曲線の描画
+      BezierCurve(canvas, points, mode).drawCurve(ts=a, te=b) # n次ベジエ曲線の描画
   else:                                  # 制御点数が既にNP個で，ピック処理
     pickid, pickdist = (0, norm(newpnt-points[0])) # ピックされた点と距離の初期化
     for i in range(1, len(points)):      # 他の制御点との比較
@@ -41,7 +47,7 @@ def dragged1(event):                     # Button1 dragged コールバック関
   if 0 <= pickid <= NP-1:                # ピックされた点の番号の確認
     points[pickid] = canvas.point(event.x, event.y) # ドラッグされた座標に変更
     canvas.clear()                       # canvasのクリア
-    BezierCurve(canvas, points, mode).drawCurve() # n次ベジエ曲線の描画
+    BezierCurve(canvas, points, mode).drawCurve(ts=a, te=b) # n次ベジエ曲線の描画
 
 def pressed2(event):                     # Button2 pressed コールバック関数
   global canvas, points                  # 大域変数 canvas, points
